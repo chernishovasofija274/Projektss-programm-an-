b94130155c0b8b2ecc3a9953713de646baae980e
chernishovasofija274 <79565100+chernishovasofija274@users.noreply.github.com>
2021-05-26 18:29:25 +0300

    Add files via upload

---
+++ Darbs_pr.py
@@ -0,0 +1,45 @@
+print("Baltkrievija,","Turcija,","Kazahstana,","Armenija,","Maroka")
+print ('Ieraksti valsti kuru gribi apmeklet  -')
+p = str(input());
+if p == 'Baltkrievija' :   
+    print ('Ko velaties uzzinat (Aprakstu, Cenas)-')
+    k = str(input());
+    if k == 'Aprakstu' :
+        print('Baltkrievija ir viena no retajam valstim Eiropa, kas ir saglabajusi neskartu dabu-bagatigus mezus, purvus, ezerus un upes... Ta ir valsts, kura lauku rajonos ir iespeja iepazit senas tradicijas, redzet krasainas sadzas majas un nogarsot nacionalo edienu- kartupeli. Savukart pilseta izjust musdienas, kas atspogulojas ekas, kuras sava neparastuma del tiek uzskatitas par arhitekturas paraugiem... Baltkrievija- zeme,kur vesture mijas ar musdienam, tradicijam un dabu.')
+    elif k == 'Cenas':
+        print ('Dienas budzets diviem: 72 USD. No ta majokli - 27 USD, partika - 30 USD, transports un izklaide - 15 USD.')
+    else: print("Uzraksti vardu pareizi ")
+elif p == 'Turcija' : 
+    print ('Ko velaties uzzinat (Aprakstu, Cenas)-')
+    k = str(input());
+    if k == 'Aprakstu' :
+        print('Turcija –paradizes sturitis zemes virsu. Turcija savaldzina ar krasno kulturu, kas saknojas nacionalajas tradicijas, ar viesmiligajiem un gurdajiem cilvekiem, kuri vienmer smaida un ir gatavi steigties paliga, ar daudzveidigo virtuvi, ko papildina muzika, legendas un austrumu pasakas. ')
+    elif k == 'Cenas':
+        print ('Dienas budzets diviem: 80 USD. No ta majokli - 30 USD, partika - 30 USD, transports un izklaide - 20 USD.')
+    else: print("Uzraksti vardu pareizi ")
+elif p == 'Kazahstana' : 
+    print ('Ko velaties uzzinat (Aprakstu, Cenas)-')
+    k = str(input());
+    if k == 'Aprakstu' :
+        print('Kazahstana ir valsts, kuru celotajam no Latvijas ir verts apmeklet un kur daudz ko redzet. Prieksstats par Kazahstanu, ka ta butu noplukusi postpadomju zeme, ir pagalam aplams. Paslaik ta ir Centralazijas lidere visas jomas, dinamiska un attistita.')
+    elif k == 'Cenas':
+        print ('Dienas budzets diviem: 85 USD. No ta majokli - 30 USD, partika - 40 USD, transports un izklaide - 15 USD.')
+    else: print("Uzraksti vardu pareizi ")
+elif p == 'Armenija' :
+    print ('Ko velaties uzzinat (Aprakstu, Cenas)-')
+    k = str(input());
+    if k == 'Aprakstu' :
+        print('Armenija ir kalnu valsts Dienvidu Kaukaza starp Melno un Kaspijas juru. Rietumos ta robezojas ar Turciju, bet ziemelos ar Gruziju. Armenija vesturiski atradas tuvu slavenajam Zida celam, lidz ar to so apgabalu ir ietekmejusas dazadu valstu kulturas un imperijas gadsimtiem ilgi. Tiesi tapec Armenijas kultura un arhitektura jutama ietekme no dazadam valstim.')
+    elif k == 'Cenas':
+        print ('Dienas budzets diviem: 77 USD. No ta majokli - 33 USD, partika - 26 USD, transports un izklaide - 18 USD.')
+    else: print("Uzraksti vardu pareizi ")
+elif p == 'Maroka' :   
+    print ('Ko velaties uzzinat (Aprakstu, Cenas)-')
+    k = str(input());
+    if k == 'Aprakstu' :
+        print('Cilveki seit ierodas, lai apskatitu Legziras marsiesu arkas, Cefsauena atstasta zilos tonus un dodas tuksnesa safari. Un vel - laizos leja no kalnu serpentina lidz Dadesas aizai un tad iedzeriet sevi ar litriem saldas piparmetru tejas. Celojumu ideju pietiks visai austrumu pasaku gramatai!')
+    elif k == 'Cenas':
+        print ('Dienas budzets diviem: 87 USD.No ta mjokli - 33 USD, partika - 27 USD, transports un izklaide - 27 USD.')
+    else: print("Uzraksti vardu pareizi ")
+print("Ja jus velaties noformet pasutijumu vai jums ir jautajumi, zvaniet pa numuru: +371 222 22 222") 
+
